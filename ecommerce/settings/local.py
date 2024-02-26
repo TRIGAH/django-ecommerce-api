bc9922f361e30a3f0ff7ab3d681a71cc89331a2c
Trigah <ijenrandy2@gmail.com>
2024-02-26 02:31:52 +0100

    testing dev branch

--- ecommerce/settings/local.py
+++ ecommerce/settings/local.py
@@ -4,7 +4,7 @@ from .base import *
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
+"default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
     }
