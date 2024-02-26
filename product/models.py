47922d0e62c4698b26125a9b65c7fb96fdd6c784
Trigah <ijenrandy2@gmail.com>
2024-02-26 18:53:47 +0100

    Many to Many Relationship with MPTT package

--- product/models.py
+++ product/models.py
@@ -1,15 +1,22 @@
 from django.db import models
+from mptt.models import MPTTModel, TreeForeignKey
 
 # Create your models here.
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
-    pass
+    parent = TreeForeignKey("self", on_delete=models.PROTECT,blank=True,null=True)
+
+    def __str__(self):
+        return self.name
 
 
 class Brand(models.Model):
     name = models.CharField(max_length=100)
-    pass
+ 
+    def __str__(self):
+        return self.name   
+
 
 
 class Product(models.Model):
@@ -17,5 +24,9 @@ class Product(models.Model):
     description = models.CharField(max_length=100,blank=True)
     is_digital = models.BooleanField(default=False)
     brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
+    category = TreeForeignKey("Category",blank=True,null=True)
+ 
+    def __str__(self):
+        return self.name   
     
 
