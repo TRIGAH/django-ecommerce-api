71e157b5e872f048d62345256fccfb0e39f1b1e2
Trigah <ijenrandy2@gmail.com>
2024-02-26 18:40:10 +0100

    Create Product models

--- product/models.py
+++ product/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    pass
+
+
+class Brand(models.Model):
+    name = models.CharField(max_length=100)
+    pass
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=100,blank=True)
+    is_digital = models.BooleanField(default=False)
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
+    
+
