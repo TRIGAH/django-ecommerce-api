c1819a4bc86978d260b7b9876029457620c7758a
Trigah <ijenrandy2@gmail.com>
2024-02-26 19:49:49 +0100

    Create viewSet to list all Categories

--- product/views.py
+++ product/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework import Response, viewSets
+
+from .models import Category
+from .serializers import CategorySerializer
 
 # Create your views here.
+
+class CategoryView(viewSets.ViewSet):
+    """
+    This view returns all the Categories
+    """
+    def list(self,request):
+        queryset = Category.objects.all()
+        serializer = CategorySerializer(queryset, many=True)
+        return Response(serializer.data)
