3a8769c07f0ce7cdc1e44a170e8ee0cdd8597b94
Trigah <ijenrandy2@gmail.com>
2024-03-02 17:06:25 +0100

    tested factory with name variable

--- ecommerce/tests/product/test_models.py
+++ ecommerce/tests/product/test_models.py
@@ -7,7 +7,7 @@ class TestCategoryModel:
     #Arrange
     def test_str_method(self, category_factory):
     #Act
-        x = category_factory()
+        x = category_factory(name="test_cat")
     #Assert
         x.__str__() == "test_category"
 
