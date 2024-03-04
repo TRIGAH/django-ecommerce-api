8720f7c29c3720faf94a7a877b049d72701b2141
Trigah <ijenrandy2@gmail.com>
2024-03-04 20:59:26 +0100

    added test for length of json response

--- ecommerce/tests/product/test_endpoints.py
+++ ecommerce/tests/product/test_endpoints.py
@@ -1,3 +1,5 @@
+import json
+
 import pytest
 
 pytestmark = pytest.mark.django_db
@@ -8,13 +10,14 @@ class TestCategoryEndpoints:
 
     def test_category_get(self, category_factory, api_client):
         #Arrange
-        category_factory()
+        category_factory.create_batch(4)
 
         #Act
         response = api_client().get(self.endpoint)
 
         #Assert
         assert response.status_code == 200
+        assert len(json.loads(response.content)) == 4
 
 
 
