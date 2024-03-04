3456473fee9ed72d51da88d92705ed0fb9ca0905
Trigah <ijenrandy2@gmail.com>
2024-03-04 20:51:16 +0100

    wrote test for category get endpoint

--- ecommerce/tests/product/test_endpoints.py
+++ ecommerce/tests/product/test_endpoints.py
@@ -1,5 +1,21 @@
+import pytest
+
+pytestmark = pytest.mark.django_db
+
 class TestCategoryEndpoints:
-    pass
+    
+    endpoint = 'api/category/'
+
+    def test_category_get(self, category_factory, api_client):
+        #Arrange
+        category_factory()
+
+        #Act
+        response = api_client().get(self.endpoint)
+
+        #Assert
+        assert response.status_code == 200
+
 
 
 class TestBrandEndpoints:
